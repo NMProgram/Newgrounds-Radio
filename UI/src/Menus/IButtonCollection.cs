607e1c2c91d06e295bdebc746c65235029d5c4c1
Nick Martens <1124834@hr.nl>
2026-09-20 19:41:41 +0200

    Change IButtonCollection indexer to method
    ..named GetButton(Index index)

--- UI/src/Menus/IButtonCollection.cs
+++ UI/src/Menus/IButtonCollection.cs
@@ -3,5 +3,5 @@ namespace NGRadio.MenuSystem;
 public interface IButtonCollection
 {
     int ButtonCount { get; }
-    IMenuButton this[int index] { get; }
+    IMenuButton GetButton(Index index);
 }
