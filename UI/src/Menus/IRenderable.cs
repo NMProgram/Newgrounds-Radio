2135d7235b9d7862ac350664a6b064113d45da5b
Nick Martens <1124834@hr.nl>
2026-09-20 20:37:59 +0200

    Document IRenderable interface

--- UI/src/Menus/IRenderable.cs
+++ UI/src/Menus/IRenderable.cs
@@ -1,6 +1,13 @@
 namespace NGRadio.MenuSystem;
 
+/// <summary>
+/// Represents an object capable of rendering an array.
+/// </summary>
 public interface IRenderable
 {
+    /// <summary>
+    /// Renders the current <see cref="IRenderable"/> instance.
+    /// </summary>
+    /// <param name="index">The array position to use for rendering.</param>
     void Render(int index);
 }
