465aad6eeb59bf955e154b04b4ebe95cdfa1c80e
Nick Martens <1124834@hr.nl>
2026-09-20 19:46:32 +0200

    Change buttons field to Buttons property
    ..shareable across classes inheriting from it

--- UI/src/Menus/Menu.cs
+++ UI/src/Menus/Menu.cs
@@ -2,15 +2,15 @@ namespace NGRadio.MenuSystem;
 
 public abstract class Menu : IRenderable, IButtonCollection
 {
-    private readonly IMenuButton[] buttons;
+    protected Menu(IMenuButton[] buttons) => this.Buttons = buttons;
 
-    protected Menu(IMenuButton[] buttons) => this.buttons = buttons;
+    protected IMenuButton[] Buttons { get; }
 
     public abstract void Render(int index);
 
-    public IMenuButton GetButton(Index index) => buttons[index];
+    public IMenuButton GetButton(Index index) => Buttons[index];
 
     public IMenuButton this[Index index] => GetButton(index);
 
-    public int ButtonCount => buttons.Length;
+    public int ButtonCount => Buttons.Length;
 }
