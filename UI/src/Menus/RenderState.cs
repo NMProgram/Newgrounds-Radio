9a4f886ea897920e72206dda786d572053045fc6
Nick Martens <1124834@hr.nl>
2026-09-20 20:45:12 +0200

    Document RenderState record

--- UI/src/Menus/RenderState.cs
+++ UI/src/Menus/RenderState.cs
@@ -1,11 +1,33 @@
 namespace NGRadio.MenuSystem;
 
+/// <summary>
+/// Holds the current history of <see cref="IRenderable"/> instances.
+/// </summary>
 public record RenderState
 {
     private readonly Stack<IRenderable> previous = [];
+
+    /// <summary>
+    /// Creates a new instance of the <see cref="RenderState"/> record.
+    /// </summary>
+    /// <param name="startRender">The first <see cref="IRenderable"/> object to store.</param>
     public RenderState(IRenderable startRender) => Current = startRender;
+
+    /// <summary>
+    /// Gets the currently active <see cref="IRenderable"/> object.
+    /// </summary>
     public IRenderable Current { get; private init; }
 
+    /// <summary>
+    /// Saves a new <see cref="IRenderable"/> instance to the state record.
+    /// </summary>
+    /// <param name="render">The render to save.</param>
     public void Save(IRenderable render) => previous.Push(render);
+
+    /// <summary>
+    /// Pops the last saved <see cref="IRenderable"/> instance from the state record.
+    /// </summary>
+    /// <returns>The last saved render, if any have been saved.</returns>
+    /// <exception cref="InvalidOperationException">Thrown when no previous render is found.</exception>
     public RenderState Pop() => this with { Current = previous.Pop() };
 }
