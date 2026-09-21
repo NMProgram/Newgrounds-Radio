f0c400379bf9efc01e36a46b86706e8fb9ba8d9a
Nick Martens <1124834@hr.nl>
2026-09-21 21:15:11 +0200

    Add MenuRunner.Start to Main

--- UI/Program.cs
+++ UI/Program.cs
@@ -1,8 +1,11 @@
 ﻿namespace NGRadio;
+
+using NGRadio.MenuSystem;
+
 static class Program
 {
     static void Main(string[] args)
     {
-        
+        MenuRunner.Start(new StartMenu());
     }
 }
