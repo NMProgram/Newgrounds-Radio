31cecd3a69e8c2aaad96f9d970be847aa71ce4b7
Nick Martens <1124834@hr.nl>
2026-09-21 19:42:12 +0200

    Fix MenuRunner's MovePosition return type
    ..with newly changed Move methods of MenuPos

--- UI/src/Menus/MenuRunner.cs
+++ UI/src/Menus/MenuRunner.cs
@@ -9,24 +9,23 @@ public static class MenuRunner
         while (state.Current is not null)
         {
             state.Current.Render(pos.Value);
-            pos = UpdatePosLimit(pos, state);
             ConsoleKeyInfo keyInfo = Console.ReadKey();
             switch (keyInfo.Key)
             {
                 case ConsoleKey.Enter: state = menu.GetButton(pos.Value).OnPress(state); break;
                 case ConsoleKey.Backspace: state = state.Pop(); break;
-                default: MovePosition(pos, keyInfo.Key); break;
+                default: pos = UpdatePosLimit(MovePosition(pos, keyInfo.Key), state); break;
             }
         }
     }
 
-    private static int MovePosition(MenuPos pos, ConsoleKey key) => key switch
+    private static MenuPos MovePosition(MenuPos pos, ConsoleKey key) => key switch
     {
         ConsoleKey.W or ConsoleKey.UpArrow => pos.MoveUp(),
         ConsoleKey.S or ConsoleKey.DownArrow => pos.MoveDown(),
         ConsoleKey.D or ConsoleKey.RightArrow => pos.MoveRight(),
         ConsoleKey.A or ConsoleKey.LeftArrow => pos.MoveLeft(),
-        _ => pos.Value
+        _ => pos
     };
 
     private static MenuPos UpdatePosLimit(MenuPos pos, RenderState state) 
