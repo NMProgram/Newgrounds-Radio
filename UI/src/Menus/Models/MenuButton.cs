f12ac7350c3a1ed2d4c080e1c553e2ad278ed3f7
Nick Martens <1124834@hr.nl>
2026-09-21 21:29:39 +0200

    Document MenuButton class

--- UI/src/Menus/Models/MenuButton.cs
+++ UI/src/Menus/Models/MenuButton.cs
@@ -1,5 +1,10 @@
 namespace NGRadio.MenuSystem;
 
+/// <summary>
+/// Represents a simple pushable button on an <see cref="IMenu"/> instance.
+/// </summary>
+/// <param name="text">The description of the button.</param>
+/// <param name="onPress">The function to invoke upon pressing the button.</param>
 public class MenuButton(string text, Func<RenderState, RenderState> onPress) : IMenuButton
 {
     public string Text => text;
