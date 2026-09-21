b2d514fe935f55abf9397237d7d2a9642827b4aa
Nick Martens <1124834@hr.nl>
2026-09-21 05:34:58 +0200

    Add MenuPos struct with logic

---
+++ Data/src/Logic/MenuPos.cs
@@ -0,0 +1,20 @@
+namespace NGRadio.Logic;
+
+using static IntUtils;
+
+public struct MenuPos
+{
+    public const int PageCount = 10;
+    public MenuPos(int value, int max) => (this.Value, this.Max) = (value, max);
+    public MenuPos(int max) : this(0, max) { }
+
+    public int Max { get; init; }
+    public int Value { get; private set; }
+
+    public int MoveUp() => Value = WrapToMax(Value - 1, Max, v => v);
+    public int MoveDown() => Value = WrapToZero(Value + 1, Max, v => v);
+    public int MoveLeft() => Value = WrapToMax(Value - PageCount, Max, FloorPageCount);
+    public int MoveRight() => Value = WrapToZero(Value + PageCount, Max, FloorPageCount);
+
+    private static int FloorPageCount(int value) => Floor(value, PageCount);
+}
