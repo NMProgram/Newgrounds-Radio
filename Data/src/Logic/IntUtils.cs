f38a1e372d7fa179f8a8845b6bd95a09f4b9c656
Nick Martens <1124834@hr.nl>
2026-09-21 05:33:43 +0200

    Change WrapToMax return summary to be clearer

--- Data/src/Logic/IntUtils.cs
+++ Data/src/Logic/IntUtils.cs
@@ -25,6 +25,6 @@ public static class IntUtils
     /// <param name="value">The value to wrap.</param>
     /// <param name="max">The maximum to compare the integer to.</param>
     /// <param name="onPositive">The selector to use if the value was positive.</param>
-    /// <returns>The maximum value minus 1 if the value was positive, otherwise the converted integer.</returns>
+    /// <returns>The maximum value decremented if the value was positive, otherwise the converted integer.</returns>
     public static int WrapToMax(int value, int max, Func<int, int> onPositive) => value >= 0 ? onPositive(value) : max - 1;
 }
