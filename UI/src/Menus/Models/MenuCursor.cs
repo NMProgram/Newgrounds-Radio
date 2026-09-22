ca8dd4c5c6018ccb6fa2bf3ce386d1a7fb64e3b6
Nick Martens <1124834@hr.nl>
2026-09-22 11:46:32 +0200

    Add MenuCursor struct
    ..implementing ICursor

---
+++ UI/src/Menus/Models/MenuCursor.cs
@@ -0,0 +1,21 @@
+namespace NGRadio.MenuSystem;
+
+using static Logic.IntUtils;
+
+public readonly struct MenuCursor : ICursor
+{
+    public const int PageSize = 10;
+
+    private readonly int max;
+    private readonly int index;
+
+    public MenuCursor(int index, int max) => (this.index, this.max) = (index, max);
+
+    public ICursor Up => new MenuCursor(WrapToZero(index + 1, max, v => v), max);
+
+    public ICursor Left => new MenuCursor(WrapToMax(index - PageSize, max, v => Floor(v, PageSize)), max);
+
+    public ICursor Down => new MenuCursor(WrapToMax(index - 1, max, v => v), max);
+
+    public ICursor Right => new MenuCursor(WrapToZero(index + PageSize, max, v => Floor(v, PageSize)), max);
+}
