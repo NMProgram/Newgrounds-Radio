9a5a656df4619d26561f160a3391ab8eebba696a
Nick Martens <1124834@hr.nl>
2026-09-22 22:18:53 +0200

    Add max only ctor in MenuCursor
    - Change Up Left Down Right properties to with expressions for clarity

--- UI/src/Menus/Models/MenuCursor.cs
+++ UI/src/Menus/Models/MenuCursor.cs
@@ -8,15 +8,16 @@ public readonly struct MenuCursor : ICursor
 
     private readonly int max;
 
-    public MenuCursor(int index, int max) => (this.Index, this.max) = (index, max);
+    public MenuCursor(int max) => this.max = max;
+    public MenuCursor(int index, int max) : this(max) => this.Index = index;
 
-    public int Index { get; }
+    public int Index { get; private init; }
 
-    public ICursor Up => new MenuCursor(WrapToMax(Index - 1, max, v => v), max);
+    public ICursor Up => this with { Index = WrapToMax(Index - 1, max, v => v) };
 
-    public ICursor Left => new MenuCursor(WrapToMax(Index - PageSize, max, v => Floor(v, PageSize)), max);
+    public ICursor Left => this with { Index = WrapToMax(Index - PageSize, max, v => Floor(v, PageSize)) };
 
-    public ICursor Down => new MenuCursor(WrapToZero(Index + 1, max, v => v), max);
+    public ICursor Down => this with { Index = WrapToZero(Index + 1, max, v => v) };
 
-    public ICursor Right => new MenuCursor(WrapToZero(Index + PageSize, max, v => Floor(v, PageSize)), max);
+    public ICursor Right => this with { Index = WrapToZero(Index + PageSize, max, v => Floor(v, PageSize)) };
 }
