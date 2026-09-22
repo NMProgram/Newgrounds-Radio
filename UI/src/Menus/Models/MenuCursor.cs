7e0972dfaf8d87e628489619805a80937d2ae382
Nick Martens <1124834@hr.nl>
2026-09-22 21:05:30 +0200

    Add Index property to MenuCursor

--- UI/src/Menus/Models/MenuCursor.cs
+++ UI/src/Menus/Models/MenuCursor.cs
@@ -7,17 +7,18 @@ public readonly struct MenuCursor : ICursor
     public const int PageSize = 10;
 
     private readonly int max;
-    private readonly int index;
 
-    public MenuCursor(int index, int max) => (this.index, this.max) = (index, max);
+    public MenuCursor(int index, int max) => (this.Index, this.max) = (index, max);
 
-    public ICursor Up => new MenuCursor(WrapToMax(index - 1, max, v => v), max);
+    public int Index { get; }
 
-    public ICursor Left => new MenuCursor(WrapToMax(index - PageSize, max, v => Floor(v, PageSize)), max);
+    public ICursor Up => new MenuCursor(WrapToMax(Index - 1, max, v => v), max);
 
-    public ICursor Down => new MenuCursor(WrapToZero(index + 1, max, v => v), max);
+    public ICursor Left => new MenuCursor(WrapToMax(Index - PageSize, max, v => Floor(v, PageSize)), max);
 
-    public ICursor Right => new MenuCursor(WrapToZero(index + PageSize, max, v => Floor(v, PageSize)), max);
+    public ICursor Down => new MenuCursor(WrapToZero(Index + 1, max, v => v), max);
 
-    public static explicit operator int(MenuCursor cursor) => cursor.index;
+    public ICursor Right => new MenuCursor(WrapToZero(Index + PageSize, max, v => Floor(v, PageSize)), max);
+
+    public static explicit operator int(MenuCursor cursor) => cursor.Index;
 }
